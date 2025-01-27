97472921899c8725cb54765f19f4035685aae947
Daniel0Marsh <danielmarsh1000@protonmail.com>
2025-01-27 15:11:59 +0000

    updating styles for smaller devices

--- source/project/routing.py
+++ source/project/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/ide/(?P<username>\w+)/(?P<project_id>\d+)/$', consumers.TerminalConsumer.as_asgi()),
+    re_path(r'ws/(?P<username>\w+)/(?P<project_id>\d+)/editor/$', consumers.TerminalConsumer.as_asgi()),
 ]
